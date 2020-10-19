95b72be6413d7c45ea9742525da2b02c79c062cf
leekt216 <leekt216@gmail.com>
2020-10-20 00:54:48 +0900

    added natspec

--- contracts/ARMOR.sol
+++ contracts/ARMOR.sol
@@ -7,10 +7,16 @@ contract ARMOR is ERC20 {
   constructor() ERC20("ARMOR", "ARMOR") public {
   }
 
+  /// @notice mint `amount` tokens to `account`
+  /// @dev only minter can call this function
+  /// @param account address to receive minted token
+  /// @param amount amount of tokens to be minted
   function mint(address account, uint256 amount) external {
     _mint(account, amount);
   }
 
+  /// @notice burn `amount` from msg.sender's balance
+  /// @param amount amount fo tokens to be burned
   function burn(uint256 amount) external {
     _burn(msg.sender, amount);
   }
