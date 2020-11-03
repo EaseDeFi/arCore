c13e6a8364590a1174e2c9a1d368af498814e8e6
Robert Forster <rforster510@yahoo.com>
2020-11-03 00:57:32 -0800

    Adjusted ArmorToken

---
+++ contracts/token/ArmorToken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+
+contract ArmorToken is ERC20 {
+
+    constructor() ERC20("Armor", "ARMOR") public {
+        _mint( msg.sender, 300000 * (10 ** 18) );
+    }
+
+}
