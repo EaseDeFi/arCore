9e7bdc6ec791fe6cd9054256d602c60fb8a9aeae
leekt216 <leekt216@gmail.com>
2020-11-07 09:58:54 +0900

    some minor fixes

--- contracts/mocks/PlanManagerMock.sol
+++ contracts/mocks/PlanManagerMock.sol
@@ -4,6 +4,7 @@ import "../interfaces/IBalanceManager.sol";
 
 contract PlanManagerMock {
   mapping(address => uint256) public prices;
+  bool coverage;
   event UpdateExpireTime(address _user);
   receive() external payable{
   }
@@ -11,10 +12,17 @@ contract PlanManagerMock {
       emit UpdateExpireTime(_user);
       _newBalance; _pricePerSec;
   }
-  function changePrice(address _balanceManager, address _user, uint256 _newPrice) external {
+  function mockChangePrice(address _balanceManager, address _user, uint256 _newPrice) external {
       IBalanceManager(_balanceManager).changePrice(_user, _newPrice);
   }
   function changePrice(address _contract, uint256 _newPrice) external {
       prices[_contract] = _newPrice;
+  }
+
+  function mockCoverage(bool _coverage) external {
+    coverage = _coverage;
+  }
+  function checkCoverage(address _user, address _sc, uint256 _time, uint256 _amount, bytes32[] calldata _path) external view returns(bool){
+    return coverage;
   }
 }
