bbed962c27040a9e2e236873a876862600e6ba85
leekt216 <leekt216@gmail.com>
2020-11-12 22:42:35 +0900

    rewardManager test

--- test/core/RewardManager.test.ts
+++ test/core/RewardManager.test.ts
@@ -15,19 +15,34 @@ describe("RewardManager", function () {
     const RewardFactory = await ethers.getContractFactory("RewardManager");
     const TokenFactory = await ethers.getContractFactory("ERC20Mock");
     rewardManager = await RewardFactory.deploy();
-    token = await TokenFactory.deploy();
     
     accounts = await ethers.getSigners(); 
     user = accounts[4];
     owner = accounts[0];
     stakeManager = accounts[1];
     rewardDistribution = accounts[2];
+    token = await TokenFactory.connect(owner).deploy();
     await rewardManager.connect(owner).initialize(token.address, await stakeManager.getAddress(), await rewardDistribution.getAddress());
   });
 
-  describe('#notifyRewardAmount()', function(){
+  describe.only('#notifyRewardAmount()', function(){
     it('should fail if msg.sender is not rewardDistribution', async function(){
-      await expect(rewardManager.notifyRewardAmount(100)).to.be.revertedWith('Caller is not reward distribution');
+      await token.connect(owner).transfer(await rewardDistribution.getAddress(), 10000);
+      await token.connect(rewardDistribution).approve(rewardManager.address, 10000);
+      await expect(rewardManager.connect(user).notifyRewardAmount(100)).to.be.revertedWith('Caller is not reward distribution');
+    });
+
+    it('should fail if token is not approved', async function(){
+      await token.connect(owner).transfer(await rewardDistribution.getAddress(), 10000);
+      await token.connect(rewardDistribution).approve(rewardManager.address, 1);
+      await expect(rewardManager.connect(rewardDistribution).notifyRewardAmount(100)).to.be.reverted;
+    });
+    
+    it('should fail increase token balance', async function(){
+      await token.connect(owner).transfer(await rewardDistribution.getAddress(), 10000);
+      await token.connect(rewardDistribution).approve(rewardManager.address, 10000);
+      await rewardManager.connect(rewardDistribution).notifyRewardAmount(100);
+      expect(await token.balanceOf(rewardManager.address)).to.equal(100);
     });
   });
 });
