932cba1a824165a958ade6e851d2524f4d93b5fc
RobertMCForster <rforster510@yahoo.com>
2021-01-22 13:07:44 -0800

    Fixed cap

--- contracts/token/ArmorToken.sol
+++ contracts/token/ArmorToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 contract ArmorToken is ERC20 {
 
     constructor() ERC20("Armor", "ARMOR") public {
-        _mint( msg.sender, 3000000000 * (10 ** 18) );
+        _mint( msg.sender, 1000000000 * (10 ** 18) );
     }
 
 }
