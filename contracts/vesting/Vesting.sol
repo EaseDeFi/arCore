9452ad99afd1d09c1571f12b2b546dba9594523e
Robert M.C. Forster <robert@armor.fi>
2021-01-26 17:42:33 -0800

    Vesting update

--- contracts/vesting/Vesting.sol
+++ contracts/vesting/Vesting.sol
@@ -1,47 +1,75 @@
 // SPDX-License-Identifier: MIT
 
+
+
 pragma solidity ^0.6.0;
 
 import "../general/Ownable.sol";
 import "../interfaces/IERC20.sol";
+import "../libraries/SafeMath.sol";
 
 contract Vesting {
+    
+    using SafeMath for uint256;
+    
     IERC20 public token;
 
+    uint256 constant private DENOMINATOR = 10000;
+
     uint256 public totalTokens;
-
     uint256 public releaseStart;
-
     uint256 public releaseEnd;
-
     uint256 public totalWeight;
 
-    mapping(address => uint256) public weight;
+    mapping (address => uint256) public starts;
+    mapping (address => uint256) public weights;
+    mapping (address => uint256) public released;
+    mapping (address => uint256) public releasable;
 
-    mapping(address => uint256) public released;
+    event Claimed(address indexed _user, uint256 _amount, uint256 _timestamp);
+    event Transfer(address indexed _from, address indexed _to, uint256 _percent, uint256 _weight, uint256 _timestamp);
 
     // do not input same recipient in the _recipients, it will lead to locked token in this contract
-    function initialize(address _token, uint256 _totalTokens, uint256 _start, uint256 _period, address[] memory _recipients, uint256[] memory _weights) public {
-        require(releaseStart == 0, "already initialized");
-        require(_recipients.length == _weights.length, "array length diff");
+    function initialize(
+                    address _token, 
+                    uint256 _totalTokens, 
+                    uint256 _start, 
+                    uint256 _period,
+                    address[] calldata _recipients, 
+                    uint256[] calldata _weights)
+      public 
+    {
+        require(releaseEnd == 0, "Contract is already initialized.");
+        require(_recipients.length == _weights.length, "Array lengths do not match.");
+        
+        releaseEnd = _start.add(_period);
         releaseStart = _start;
-        releaseEnd = _start + _period;
         token = IERC20(_token);
         token.transferFrom(msg.sender, address(this), _totalTokens);
         totalTokens = _totalTokens;
         uint256 sum = 0;
-        for(uint256 i = 0; i<_recipients.length; i++){
-            weight[_recipients[i]] = _weights[i];
-            sum += _weights[i];
+        
+        for(uint256 i = 0; i<_recipients.length; i++) {
+            starts[_recipients[i]] = releaseStart;
+            weights[_recipients[i]] = _weights[i];
+            sum = sum.add(_weights[i]);
         }
+        
         totalWeight = sum;
     }
 
-    function claim() external {
-        require(releaseStart <= block.timestamp, "relase not started");
-        uint256 claimAmount = claimableAmount(msg.sender);
-        released[msg.sender] += claimAmount;
-        token.transfer(msg.sender, claimAmount);
+    function claim()
+      public
+    {
+        address user = msg.sender;
+        require(releaseStart <= block.timestamp, "Release has not started");
+
+        uint256 claimAmount = claimableAmount(user);
+        released[user] = released[user].add(claimAmount);
+        releasable[user] = 0;
+        token.transfer(user, claimAmount);
+        
+        emit Claimed(user, claimAmount, block.timestamp);
     }
 
     function claimableAmount(address _user) public view returns(uint256) {
@@ -49,7 +77,37 @@ contract Vesting {
             return 0;
         }
         uint256 applicableTimeStamp = block.timestamp >= releaseEnd ? releaseEnd : block.timestamp;
-        uint256 totalClaimable = totalTokens * (applicableTimeStamp - releaseStart) * weight[_user] / (( releaseEnd - releaseStart ) * totalWeight);
-        return totalClaimable - released[_user];
+        uint256 totalClaimable = totalTokens * (applicableTimeStamp.sub(starts[_user])) * weights[_user] / (( releaseEnd.sub(releaseStart) ) * totalWeight);
+        return totalClaimable.sub(released[_user]).add(releasable[_user]);
     }
+    
+    /**
+     * @dev Transfers a sender's weight to another address starting from now.
+     * @param _to The address to transfer weight to.
+     * @param _percentInHundredths The percent of your weight to transfer (1000 = 10%).
+    **/
+    function transfer(address _to, uint256 _percentInHundredths)
+      external
+    {
+        address user = msg.sender;
+        require(_to != address(0), "You may not transfer funds to address(0)");
+        require(weights[user] > 0 && weights[_to] == 0, "User has no stake or recipient has stake.");
+        require(_percentInHundredths <= 10000, "You may not transfer more than 100% of your weight.");
+        require(block.timestamp <= releaseEnd && block.timestamp >= releaseStart, "Must only transfer within vesting period.");
+        
+        // Clear claimable tokens first without transferring out of the contract.
+        releasable[user] = claimableAmount(user);
+        
+        uint256 amount = weights[user].mul(_percentInHundredths).div(DENOMINATOR);
+        weights[user] = weights[user].sub(amount);
+        weights[_to] = weights[_to].add(amount);
+        
+        // User must also be reset.
+        starts[user] = block.timestamp;
+        starts[_to] = block.timestamp;
+        released[user] = 0;
+        
+        emit Transfer(user, _to, _percentInHundredths, amount, block.timestamp);
+    }
+
 }
--- test/general/Vesting.test.ts
+++ test/general/Vesting.test.ts
@@ -14,8 +14,8 @@ describe("Vesting", function(){
   let weights: number[];
   beforeEach(async function(){
     const accounts = await ethers.getSigners();
-    recipients = accounts.slice(1, 10 + 1);
-    weights = [1,2,3,4,5,6,7,8,9,10];
+    recipients = accounts.slice(1, 11 + 1);
+    weights = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,0];
     const Vesting = await ethers.getContractFactory("Vesting");
     const ERC20 = await ethers.getContractFactory("ERC20Mock");
     token = await ERC20.deploy();
@@ -29,7 +29,7 @@ describe("Vesting", function(){
       await expect(vesting.initialize(token.address, totalAmount, now.add(100), period,recipients, weights)).to.be.reverted;
     });
     it("should set appropriate values", async function(){
-      expect(await vesting.totalWeight()).to.equal(55);
+      expect(await vesting.totalWeight()).to.equal(55000);
       expect(await vesting.token()).to.equal(token.address);
       expect(await vesting.totalTokens()).to.equal(totalAmount);
     });
@@ -37,7 +37,7 @@ describe("Vesting", function(){
 
   describe("#claimableAmount()", function(){
     it("should return 0 if release has not started", async function(){
-      for(let i = 0; i<recipients.length; i++){
+      for(let i = 0; i<recipients.length - 1 ; i++){
         const amount = await vesting.claimableAmount(recipients[i].getAddress());
         expect(amount).to.equal(BigNumber.from(0));
       }
@@ -47,7 +47,7 @@ describe("Vesting", function(){
       await increaseTo(now.add(100).toNumber());
       await increase(100);
       await mine();
-      for(let i = 0; i<recipients.length; i++){
+      for(let i = 0; i<recipients.length - 1; i++){
         const expectedAmount = totalAmount.mul(i+1).div(55).div(100);
         const amount = await vesting.claimableAmount(recipients[i].getAddress());
         expect(amount).to.equal(expectedAmount);
@@ -58,12 +58,12 @@ describe("Vesting", function(){
       await increaseTo(now.add(100).toNumber());
       await increase(100);
       await mine();
-      for(let i = 0; i<recipients.length; i++){
+      for(let i = 0; i<recipients.length - 1; i++){
         await vesting.connect(recipients[i]).claim();
       }
       await increaseTo(now.add(300).toNumber());
       await mine();
-      for(let i = 0; i<recipients.length; i++){
+      for(let i = 0; i<recipients.length - 1; i++){
         const expectedAmount = totalAmount.mul(i+1).div(55).div(50);
         const amount = await vesting.claimableAmount(recipients[i].getAddress());
         expect((await token.balanceOf(recipients[i].getAddress())).add(amount)).to.equal(expectedAmount);
@@ -84,7 +84,7 @@ describe("Vesting", function(){
       expect(await token.balanceOf(recipients[0].getAddress())).to.equal(expectedAmount);
     });
 
-    it("should release appropriate vlaues even if there was a claim", async function(){
+    it("should release appropriate values even if there was a claim", async function(){
       await increaseTo(now.add(100).toNumber());
       await increase(100);
       await vesting.connect(recipients[0]).claim();
@@ -95,4 +95,42 @@ describe("Vesting", function(){
       expect(await token.balanceOf(recipients[0].getAddress())).to.equal(expectedAmount);
     });
   });
+
+  describe("#transfer()", async function(){
+    it("should fail if release is not started yet", async function(){
+      await expect(vesting.connect(recipients[0]).transfer(recipients[1],1000)).to.be.reverted;
+    });
+
+    it("should transfer the appropriate values", async function(){
+      await increaseTo(now.add(100).toNumber());
+      await increase(300);
+      await vesting.connect(recipients[0]).transfer(recipients[10].getAddress(),1000);
+      const expectedAmount = totalAmount.mul(3).div(55).div(100);
+      expect(await vesting.releasable(recipients[0].getAddress())).to.equal(expectedAmount);
+      expect(await vesting.weights(recipients[10].getAddress())).to.equal(100);
+      expect(await vesting.weights(recipients[0].getAddress())).to.equal(900);
+    });
+
+    it("should release appropriate values after a transfer", async function(){
+      await increaseTo(now.add(100).toNumber());
+      await increase(100);
+      await vesting.connect(recipients[0]).transfer(recipients[10].getAddress(),1000);
+
+      await increaseTo(now.add(100).toNumber());
+      await increase(100);
+
+      await vesting.connect(recipients[0]).claim();
+      await vesting.connect(recipients[10]).claim();
+
+      expect(await vesting.released(recipients[0].getAddress())).to.equal(totalAmount.mul(1).div(55).div(100));
+      await vesting.connect(recipients[0]).claim();
+      await vesting.connect(recipients[10]).claim();
+      const expectedAmount = totalAmount.mul(9).div(550).div(1000);
+      const expectedAmount2 = totalAmount.mul(1).div(550).div(1000);
+
+      expect(await token.balanceOf(recipients[0].getAddress())).to.equal(expectedAmount);
+      expect(await token.balanceOf(recipients[10].getAddress())).to.equal(expectedAmount2);
+    });
+  });
+
 });
