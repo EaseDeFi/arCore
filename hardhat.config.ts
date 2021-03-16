4ec4d13b20e93ee090f92d9676904f37dd65bf8e
leekt <leekt216@gmail.com>
2021-03-16 20:40:41 +0900

    hotfix test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -47,7 +47,7 @@ export default {
       timeout: 1000000,
       forking: {
         url: "https://eth-mainnet.alchemyapi.io/v2/90dtUWHmLmwbYpvIeC53UpAICALKyoIu",
-        blockNumber: 12048568
+        blockNumber: 12049335
       }
     },
     coverage: {
